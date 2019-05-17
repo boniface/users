c494f5fec47ae22415af1f3c87fdaad513d85e24
boniface <boniface@kabaso.com>
2019-05-17 18:52:46 +0200

    Refactored Page templates

--- cmd/web/controllers/roles/roles_controller.go
+++ cmd/web/controllers/roles/roles_controller.go
@@ -21,7 +21,6 @@ func RolesHanler(app *config.Env) http.HandlerFunc {
 			"./views/html/base/base.page.html",
 			"./views/html/base/footer.page.html",
 			"./views/html/base/header.page.html",
-			"./views/html/base/modal.page.html",
 			"./views/html/base/navbar.page.html",
 			"./views/html/base/sidebar.page.html",
 			"./views/html/base/tabs.page.html",
