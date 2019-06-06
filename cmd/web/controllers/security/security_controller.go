9e81af971c71a90c5d3797363f48989c66881015
christian <muamba90@gmail.com>
2019-06-06 23:58:26 +0200

    adding route for the pages

--- cmd/web/controllers/security/security_controller.go
+++ cmd/web/controllers/security/security_controller.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"users/cmd/config"
+	"users/pkg/api/security"
 )
 
 func Security(app *config.Env) http.Handler {
@@ -16,15 +17,39 @@ func Security(app *config.Env) http.Handler {
 
 func SecurityHanler(app *config.Env) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		apiKeys, err := security.GetApiKeys()
+
+		if err != nil {
+			app.ServerError(w, err)
+		}
+
+		siteAccessKeys, err := security.GetSiteAccessKeysApis()
+
+		if err != nil {
+			app.ServerError(w, err)
+		}
+
+		type PageData struct {
+			apiKey             []security.ApiKeys
+			siteAccessKeysApis []security.SiteAccessKeysApi
+			Name               string
+		}
+		data := PageData{apiKeys, siteAccessKeys, " "}
+
 		files := []string{
-			"./views/html/security/security.page.html",
+			app.Path + "/security/security.page.html",
+			app.Path + "/base/base.page.html",
+			app.Path + "/base/navbar.page.html",
+			app.Path + "/base/sidebar.page.html",
+			app.Path + "/base/footer.page.html",
 		}
 		ts, err := template.ParseFiles(files...)
 		if err != nil {
 			app.ErrorLog.Println(err.Error())
 			return
 		}
-		err = ts.Execute(w, nil)
+		err = ts.ExecuteTemplate(w, "base", data)
 		if err != nil {
 			app.ErrorLog.Println(err.Error())
 		}
